d572c57b15e19530edc3db9e54c5fd5ab8095218
wl <waldervon@gmail.com>
2026-06-03 17:29:51 +0600

    update in 3th june

--- hyperv.py
+++ hyperv.py
@@ -1,6 +1,26 @@
+#                                                                            
+#                                                                            
+#    EEEEEEEEEEEEEEEEEEEEEEHHHHHHHHH     HHHHHHHHHVVVVVVVV           VVVVVVVV
+#    E::::::::::::::::::::EH:::::::H     H:::::::HV::::::V           V::::::V
+#    E::::::::::::::::::::EH:::::::H     H:::::::HV::::::V           V::::::V
+#    EE::::::EEEEEEEEE::::EHH::::::H     H::::::HHV::::::V           V::::::V
+#      E:::::E       EEEEEE  H:::::H     H:::::H   V:::::V           V:::::V 
+#      E:::::E               H:::::H     H:::::H    V:::::V         V:::::V  
+#      E::::::EEEEEEEEEE     H::::::HHHHH::::::H     V:::::V       V:::::V   
+#      E:::::::::::::::E     H:::::::::::::::::H      V:::::V     V:::::V    
+#      E:::::::::::::::E     H:::::::::::::::::H       V:::::V   V:::::V     
+#      E::::::EEEEEEEEEE     H::::::HHHHH::::::H        V:::::V V:::::V      
+#      E:::::E               H:::::H     H:::::H         V:::::V:::::V       
+#      E:::::E       EEEEEE  H:::::H     H:::::H          V:::::::::V        
+#    EE::::::EEEEEEEE:::::EHH::::::H     H::::::HH         V:::::::V         
+#    E::::::::::::::::::::EH:::::::H     H:::::::H          V:::::V          
+#    E::::::::::::::::::::EH:::::::H     H:::::::H           V:::V           
+#    EEEEEEEEEEEEEEEEEEEEEEHHHHHHHHH     HHHHHHHHH            VVV            
+#                                                                            
+# EasyHyperV - by Wl13Proger9
+# https://github.com/Wl13proger9/EasyHyperV       
+                                                                                                                                                                                                                                                                                                                      
 import subprocess
-
-
 
 
 
@@ -8,14 +28,43 @@ import subprocess
 
 class HyperVisor:
     def __init__(self):
-        result = subprocess.run(
-            ["bcdedit", "/enum"],
+        def run_from_powershell(command:str, capture_type:type = bool) -> str:
+            result = subprocess.run(
+            [ "powershell", "-Command", command ],
             capture_output=True,
-            text=True,
-            #encoding="utf-8"
-            )
+            text=True
+            )          
+            
+            if capture_type == bool:
+                return result.stdout.strip().lower() == "true"
+            
+
+            elif capture_type == dict:
+                info = {}
+                for line in result.stdout.splitlines():
+                    if ":" in line:
+                        key, value = line.split(":", 1)
+                        info[key.strip()] = value.strip()
+                            
+                return info 
+            
+
+            elif capture_type == str:
+                return result.stdout.strip()
+            
+    
+            else:
+                return result
+        
 
         options = {}
+
+
+        result = subprocess.run(
+            ["bcdedit", "/enum"],
+            capture_output=True,text=True,  
+            )
+
 
         for line in result.stdout.splitlines():
             parts = line.split()
@@ -24,28 +73,26 @@ class HyperVisor:
                 value = " ".join(parts[1:])
                 options[key] = value
 
-        secureboot = subprocess.run(
-            [
-                "powershell",
-                "-Command",
-                "Confirm-SecureBootUEFI"
-            ],
-            capture_output=True,
-            text=True
-            ).stdout.strip().lower() == "true"
-        
-        self.secureboot_status = secureboot
-        self.options           = options
 
-
-    def check_system(self) -> dict:
+        self.options             = options
+        self.run_from_powershell = run_from_powershell
+        self.secureboot_status   = run_from_powershell('Confirm-SecureBootUEFI')
         
+
+    def check_system(self) -> dict:    
         output = {
-            "loadoptions":          self.options.get("loadoptions"),                
-            "hypervisorlaunchtype": self.options.get("hypervisorlaunchtype"),
-            "locale":               self.options.get("locale"),
-            "secureboot":           self.secureboot_status
-            
+            "loadoptions":                          self.options.get("loadoptions"),                
+            "hypervisorlaunchtype":                 self.options.get("hypervisorlaunchtype"),
+            "locale":                               self.options.get("locale"),
+            "secureboot":                           self.secureboot_status,
+            "VirtualizationFirmware":               self.run_from_powershell('(Get-CimInstance -ClassName Win32_Processor).VirtualizationFirmwareEnabled'),
+            "Manufacturer":                        [self.run_from_powershell('Get-CimInstance Win32_BaseBoard', dict)['Manufacturer'], 
+                                                    self.run_from_powershell('Get-CimInstance Win32_BaseBoard', dict)['Product']],
+            "HypervisorPresent":                    self.run_from_powershell('Get-CimInstance Win32_ComputerSystem).HypervisorPresent'),
+            "ModeExtensions":                       self.run_from_powershell('(Get-CimInstance Win32_Processor).VMMonitorModeExtensions'),
+            "VirtualizationFirmwareEnabled":        self.run_from_powershell('(Get-CimInstance Win32_Processor).VirtualizationFirmwareEnabled'),
+            "ProcessorName":                        self.run_from_powershell('(Get-CimInstance Win32_Processor).Name', str)
+                                        
         }
         
         return output
@@ -53,16 +100,14 @@ class HyperVisor:
 
 
 
-
 if __name__ == "__main__":
     h = HyperVisor()
 
-
     print( h.check_system() )
 
-    for key in h.check_system():
-        print(f"\n{key}:_____{h.check_system()[key]}______{type( h.check_system()[key] )}")
+    #for key in h.check_system():print(f"{ key }:    { h.check_system()[key] }")
 
+    
 
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
-HyperV 02/06/2026
+EEEEEEEEEEEEEEEEEEEEEEHHHHHHHHH     HHHHHHHHHVVVVVVVV           VVVVVVVV
+E::::::::::::::::::::EH:::::::H     H:::::::HV::::::V           V::::::V
+E::::::::::::::::::::EH:::::::H     H:::::::HV::::::V           V::::::V
+EE::::::EEEEEEEEE::::EHH::::::H     H::::::HHV::::::V           V::::::V
+  E:::::E       EEEEEE  H:::::H     H:::::H   V:::::V           V:::::V 
+  E:::::E               H:::::H     H:::::H    V:::::V         V:::::V  
+  E::::::EEEEEEEEEE     H::::::HHHHH::::::H     V:::::V       V:::::V   
+  E:::::::::::::::E     H:::::::::::::::::H      V:::::V     V:::::V    
+  E:::::::::::::::E     H:::::::::::::::::H       V:::::V   V:::::V     
+  E::::::EEEEEEEEEE     H::::::HHHHH::::::H        V:::::V V:::::V      
+  E:::::E               H:::::H     H:::::H         V:::::V:::::V       
+  E:::::E       EEEEEE  H:::::H     H:::::H          V:::::::::V        
+EE::::::EEEEEEEE:::::EHH::::::H     H::::::HH         V:::::::V         
+E::::::::::::::::::::EH:::::::H     H:::::::H          V:::::V          
+E::::::::::::::::::::EH:::::::H     H:::::::H           V:::V           
+EEEEEEEEEEEEEEEEEEEEEEHHHHHHHHH     HHHHHHHHH            VVV            
